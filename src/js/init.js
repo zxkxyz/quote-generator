0343961843b88fb74990e299ff3ac8730d92195d
Zak Golding <hello@zakarhino.com>
2016-01-12 22:42:16 -0800

    Basic working static site

---
+++ src/js/init.js
@@ -0,0 +1,10 @@
+$(function() {
+  var quotes = [
+    "juh-queer-ee",
+    "It'll tell me tiddlywinks... if I set it to tiddlywinks.",
+    "Your eyes are going to deceive you - you need to turn off your brain."
+  ];
+
+  var randIndex = Math.floor(Math.random() * quotes.length);
+  $("#quote").text('"' + quotes[randIndex] + '"');
+});
