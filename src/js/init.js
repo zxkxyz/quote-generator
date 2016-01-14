783aeefabfd17f2b3f34e4271ba44abe24ef4010
Zak Golding <hello@zakarhino.com>
2016-01-13 16:47:03 -0800

    Fixed quotes array

--- src/js/init.js
+++ src/js/init.js
@@ -4,7 +4,7 @@ $(function() {
     "It'll tell me tiddlywinks... if I set it to tiddlywinks.",
     "Your eyes are going to deceive you - you need to turn off your brain.",
     "lexical is a 50 cent word",
-    "arr... arr arr... arr arr arr..."",
+    "arr... arr arr... arr arr arr...",
     "language is important",
     "recursion is not real in some ways",
     "let me pull up our heiristic for determining the future"
